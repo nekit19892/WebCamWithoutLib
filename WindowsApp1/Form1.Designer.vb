50a358625f4404d5a8251349873072113d5e4491
Сергеев Никита <letter20016@gmail.com>
2019-06-13 20:45:54 +0300

    Добавьте файлы проекта.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(471, 51)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(108, 56)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Запустить"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(471, 211)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(108, 65)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Остановить"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(42, 22)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(371, 297)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 356)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "WebCamCapt"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,98 @@
+﻿Public Class Form1
+
+    Const WM_CAP As Short = &H400S
+    Const WM_CAP_DRIVER_CONNECT As Integer = WM_CAP + 10
+    Const WM_CAP_DRIVER_DISCONNECT As Integer = WM_CAP + 11
+
+    Const WM_CAP_SET_PREVIEW As Integer = WM_CAP + 50
+    Const WM_CAP_SET_PREVIEWRATE As Integer = WM_CAP + 52
+    Const WM_CAP_SET_SCALE As Integer = WM_CAP + 53
+    Const WS_CHILD As Integer = &H40000000
+    Const WS_VISIBLE As Integer = &H10000000
+    Const SWP_NOMOVE As Short = &H2S
+    Const SWP_NOZORDER As Short = &H4S
+    Const HWND_BOTTOM As Short = 1
+
+    Dim iDevice As Integer = 0
+    Dim hHwnd As Integer
+
+    Declare Function SendMessage Lib "user32" Alias "SendMessageA" _
+     (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Boolean,
+     ByRef lParam As Integer) As Boolean
+
+    Declare Function SetWindowPos Lib "user32" Alias "SetWindowPos" (ByVal hwnd As Integer,
+        ByVal hWndInsertAfter As Integer, ByVal x As Integer, ByVal y As Integer,
+        ByVal cx As Integer, ByVal cy As Integer, ByVal wFlags As Integer) As Integer
+
+    Declare Function DestroyWindow Lib "user32" (ByVal hndw As Integer) As Boolean
+
+    Declare Function capCreateCaptureWindowA Lib "avicap32.dll" _
+         (ByVal lpszWindowName As String, ByVal dwStyle As Integer,
+         ByVal x As Integer, ByVal y As Integer, ByVal nWidth As Integer,
+         ByVal nHeight As Short, ByVal hWndParent As Integer,
+         ByVal nID As Integer) As Integer
+
+    Declare Function capGetDriverDescriptionA Lib "avicap32.dll" (ByVal wDriver As Short,
+        ByVal lpszName As String, ByVal cbName As Integer, ByVal lpszVer As String,
+        ByVal cbVer As Integer) As Boolean
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If Button2.Enabled Then
+            ClosePreviewWindow()
+        End If
+    End Sub
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadDeviceList()
+        Button1.Enabled = True
+        Me.AutoScrollMinSize = New Size(100, 100)
+        Button2.Enabled = False
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        If Button2.Enabled = False Then
+            PictureBox1.Hide()
+            Me.Width = 650
+            Me.Height = 450
+        End If
+    End Sub
+    Private Sub LoadDeviceList()
+        Dim strName As String = Space(100)
+        Dim strVer As String = Space(100)
+        Dim bReturn As Boolean
+        Dim x As Short = 0
+        Do
+            bReturn = capGetDriverDescriptionA(x, strName, 100, strVer, 100)
+            x += CType(1, Short)
+        Loop Until bReturn = False
+    End Sub
+
+    Private Sub OpenPreviewWindow()
+        hHwnd = capCreateCaptureWindowA(iDevice.ToString, WS_VISIBLE Or WS_CHILD, 0, 0, 1280,
+            720, PictureBox1.Handle.ToInt32, 0)
+        If SendMessage(hHwnd, WM_CAP_DRIVER_CONNECT, iDevice, 0) Then
+            SendMessage(hHwnd, WM_CAP_SET_SCALE, True, 0)
+            SendMessage(hHwnd, WM_CAP_SET_PREVIEWRATE, 2, 0)
+            SendMessage(hHwnd, WM_CAP_SET_PREVIEW, True, 0)
+            SetWindowPos(hHwnd, HWND_BOTTOM, 0, 0, PictureBox1.Width, PictureBox1.Height,
+                    SWP_NOMOVE Or SWP_NOZORDER)
+            Button2.Enabled = True
+            Button1.Enabled = True
+        Else
+            DestroyWindow(hHwnd)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenPreviewWindow()
+        PictureBox1.Show()
+    End Sub
+
+    Private Sub ClosePreviewWindow()
+        SendMessage(hHwnd, WM_CAP_DRIVER_DISCONNECT, iDevice, 0)
+        DestroyWindow(hHwnd)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        ClosePreviewWindow()
+        Button2.Enabled = False
+    End Sub
+
+End Class
